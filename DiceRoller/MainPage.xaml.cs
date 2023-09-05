6a21e704ca32eabc894d25d7bc83bd8cc72a392a
unknown <carlos.edusilva7899@gmail.com>
2023-09-05 08:18:39 -0300

    commit final do projeto

--- DiceRoller/MainPage.xaml.cs
+++ DiceRoller/MainPage.xaml.cs
@@ -1,24 +1,43 @@
-﻿namespace DiceRoller;
+﻿using System.ComponentModel.Design;
+
+namespace DiceRoller;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    Random rnd = new Random();
+    public void OnButtonClicked(object sender, EventArgs args)
+    {
+        string opcao = DiceOptionsPicker.SelectedItem as string;
+        if (opcao == "4")
+        {
+            int sorte = rnd.Next(1, 5);
+            CounterBtn.Text = $"{sorte}";
+        }else if (opcao == "6") 
+        {
+            int sorte = rnd.Next(1, 7);
+            CounterBtn.Text = $"{sorte}";
+        }else if (opcao == "10")
+        {
+            int sorte = rnd.Next(1, 11);
+            CounterBtn.Text = $"{sorte}";
+        }else if (opcao == "20")
+        {
+            int sorte = rnd.Next(1, 21);
+            CounterBtn.Text = $"{sorte}";
+        }else
+        {
+            int sorte = rnd.Next(1, 101);
+            CounterBtn.Text = $"{sorte}";
+        }
+    }
 }
+
+   
+//if (ctr % 5 == 0)
 
